77b05b3d881a46f746f9ec8520b3b2ab350b2bdd
James Wright <jwizzle.uk@gmail.com>
2017-12-15 00:17:33 +0000

    Setup basic architecture

--- SimRunner/MainWindow.xaml.cs
+++ SimRunner/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using AutoJunctSim;
 
 namespace SimRunner
 {
@@ -23,6 +24,14 @@ namespace SimRunner
         public MainWindow()
         {
             InitializeComponent();
+            Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            var simulation = new AutoJunctSimulation();
+            var renderer = new Renderer(m_Canvas, simulation);
+            renderer.Start();
         }
     }
 }
---
+++ SimRunner/Renderer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using AutoJunctSim;
+
+namespace SimRunner
+{
+    public class Renderer
+    {
+        private Canvas m_Canvas;
+        private AutoJunctSimulation m_Simulation;
+        public Renderer(Canvas canvas, AutoJunctSimulation simulation)
+        {
+            m_Canvas = canvas;
+            m_Simulation = simulation;
+        }
+
+        public void Start()
+        {
+
+        }
+    }
+}
