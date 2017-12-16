f6fbc6b030d88a048764c483782403ab35af226b
James Wright <jwizzle.uk@gmail.com>
2017-12-16 15:54:46 +0000

    Put dummy vehicle in simulation

--- AutoJunctSim/AutoJunctSimulation.cs
+++ AutoJunctSim/AutoJunctSimulation.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SpatialTypes;
 
 namespace AutoJunctSim
 {
@@ -16,6 +17,13 @@ namespace AutoJunctSim
         public IList<IVehicleSprite> Vehicles
         {
             get; private set;
-        } = new List<IVehicleSprite>();
+        } = new List<IVehicleSprite>() { new DummyVehicleSprite() };
+    
+        private class DummyVehicleSprite : IVehicleSprite
+        {
+            public Point CentrePoint { get; } = new Point(0, 0);
+            public Size Size { get; } = new Size(2, 5);
+            public Angle Heading { get; } = Angle.FromDegrees(135);
+        }
     }
 }
