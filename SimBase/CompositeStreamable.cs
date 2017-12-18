fe07b0602c86817b7449ca661136255f52ab645f
James Wright <jwizzle.uk@gmail.com>
2017-12-18 21:52:54 +0000

    Separate data structure from business logic

--- SimBase/CompositeStreamable.cs
+++ SimBase/CompositeStreamable.cs
@@ -9,98 +9,28 @@ namespace SimBase
 {
     public class CompositeStreamable<T> : IStreamable<T>
     {
-        private LinkList<Tuple<double, IStreamable<T>>> m_Streamables;
+        private DoubleRangeMap<IStreamable<T>> m_Streamables;
 
         public CompositeStreamable()
         {
-            m_Streamables = new LinkList<Tuple<double, IStreamable<T>>>(new Tuple<double, IStreamable<T>>(0, null));
+            m_Streamables = new DoubleRangeMap<IStreamable<T>>(null);
         }
 
         public void Add(IStreamMoment start, IStreamMoment end, IStreamable<T> streamable)
         {
-            if(start.PositionInStream >= end.PositionInStream)
-            {
-                throw new ArgumentOutOfRangeException(nameof(end), end, "Must be greater than start position");
-            }
-            if(start.PositionInStream < 0)
-            {
-                throw new ArgumentOutOfRangeException(nameof(start), start, "Must be greater than or equal to zero");
-            }
-            if(end.PositionInStream <= 0)
-            {
-                throw new ArgumentOutOfRangeException(nameof(end), end, "Must be greater than zero");
-            }
-
-            var lastStreamableBeforeNew = m_Streamables;
-            while(lastStreamableBeforeNew.Next != null && lastStreamableBeforeNew.Next.Value.Item1 < start.PositionInStream)
-            {
-                lastStreamableBeforeNew = lastStreamableBeforeNew.Next;
-            }
-
-            var firstStreamableAfterNew = lastStreamableBeforeNew;
-            while (firstStreamableAfterNew.Next != null && firstStreamableAfterNew.Next.Value.Item1 < end.PositionInStream)
-            {
-                firstStreamableAfterNew = firstStreamableAfterNew.Next;
-            }
-
-            var newNode = new LinkList<Tuple<double, IStreamable<T>>>(new Tuple<double, IStreamable<T>>(start.PositionInStream, streamable));
-
-            newNode.Next = new LinkList<Tuple<double, IStreamable<T>>>(new Tuple<double, IStreamable<T>>(end.PositionInStream, firstStreamableAfterNew.Value.Item2));
-            newNode.Next.Next = firstStreamableAfterNew.Next;
-
-            lastStreamableBeforeNew.Next = newNode;
+            m_Streamables.Add(start.PositionInStream, end.PositionInStream, streamable);
         }
 
         public void Add(IStreamMoment start, IStreamable<T> streamable)
         {
-            if(start.PositionInStream < 0)
-            {
-                throw new ArgumentOutOfRangeException(nameof(start), start, "Must be greater than or equal to zero");
-            }
-
-            var lastStreamableBeforeNew = m_Streamables;
-            while(lastStreamableBeforeNew.Next != null && lastStreamableBeforeNew.Next.Value.Item1 < start.PositionInStream)
-            {
-                lastStreamableBeforeNew = lastStreamableBeforeNew.Next;
-            }
-
-            var newNode = new LinkList<Tuple<double, IStreamable<T>>>(new Tuple<double, IStreamable<T>>(start.PositionInStream, streamable));
-
-            lastStreamableBeforeNew.Next = newNode;
+            m_Streamables.Add(start.PositionInStream, streamable);
         }
 
         public T AtMoment(IStreamMoment moment)
         {
-            var presentMoment = moment.PositionInStream;
-            IStreamable<T> streamable = m_Streamables.Where(t => t.Item1 <= presentMoment).LastOrDefault()?.Item2;
+            var streamable = m_Streamables[moment.PositionInStream];
             if (streamable == null) throw new ArgumentException("No streamable has been given for that stream moment");
             return streamable.AtMoment(moment);
-        }
-
-        private class LinkList<TList> : IEnumerable<TList>
-        {
-            public TList Value;
-            public LinkList<TList> Next = null;
-
-            public IEnumerator<TList> GetEnumerator()
-            {
-                return GetValues().GetEnumerator();
-            }
-
-            IEnumerator IEnumerable.GetEnumerator()
-            {
-                return GetEnumerator();
-            }
-
-            private IEnumerable<TList> GetValues()
-            {
-                return new TList[] { Value }.Concat(Next?.GetValues() ?? Enumerable.Empty<TList>());
-            }     
-            
-            public LinkList(TList head)
-            {
-                Value = head;
-            }       
         }
     }
 }
---
+++ SimBase/DoubleRangeMap.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SimBase
+{
+    public class DoubleRangeMap<T>
+    {
+        private LinkList<Tuple<double, T>> m_Items;
+        public DoubleRangeMap(T defaultValue = default(T))
+        {
+            m_Items = new LinkList<Tuple<double, T>>(new Tuple<double, T>(double.MinValue, defaultValue));
+        }
+
+        public void Add(double start, double end, T item)
+        {
+            if (start >= end)
+            {
+                throw new ArgumentOutOfRangeException(nameof(end), end, $"Must be greater than {nameof(start)}");
+            }
+
+            var lastItemBeforeNew = m_Items;
+            while (lastItemBeforeNew.Next != null && lastItemBeforeNew.Next.Value.Item1 < start)
+            {
+                lastItemBeforeNew = lastItemBeforeNew.Next;
+            }
+
+            var firstItemAfterNew = lastItemBeforeNew;
+            while (firstItemAfterNew.Next != null && firstItemAfterNew.Next.Value.Item1 < end)
+            {
+                firstItemAfterNew = firstItemAfterNew.Next;
+            }
+
+            var newNode = new LinkList<Tuple<double, T>>(new Tuple<double, T>(start, item));
+
+            newNode.Next = new LinkList<Tuple<double, T>>(new Tuple<double, T>(end, firstItemAfterNew.Value.Item2));
+            newNode.Next.Next = firstItemAfterNew.Next;
+
+            lastItemBeforeNew.Next = newNode;
+        }
+
+        public void Add(double start, T item)
+        {
+            var lastItemBeforeNew = m_Items;
+            while (lastItemBeforeNew.Next != null && lastItemBeforeNew.Next.Value.Item1 < start)
+            {
+                lastItemBeforeNew = lastItemBeforeNew.Next;
+            }
+
+            var newNode = new LinkList<Tuple<double, T>>(new Tuple<double, T>(start, item));
+
+            lastItemBeforeNew.Next = newNode;
+        }
+
+        public T this[double position]
+        {
+            get
+            {
+                var pointer = m_Items;
+                while (pointer.Next != null && pointer.Next.Value.Item1 <= position)
+                {
+                    pointer = pointer.Next;
+                }
+                return pointer.Value.Item2;
+            }
+        }
+
+        private class LinkList<TList> : IEnumerable<TList>
+        {
+            public TList Value;
+            public LinkList<TList> Next = null;
+
+            public IEnumerator<TList> GetEnumerator()
+            {
+                return GetValues().GetEnumerator();
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                return GetEnumerator();
+            }
+
+            private IEnumerable<TList> GetValues()
+            {
+                return new TList[] { Value }.Concat(Next?.GetValues() ?? Enumerable.Empty<TList>());
+            }
+
+            public LinkList(TList head)
+            {
+                Value = head;
+            }
+        }
+    }
+}
