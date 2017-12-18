f1e23c3fb9ad2ac7676d96c37e603d2e6f6a7d39
James Wright <jwizzle.uk@gmail.com>
2017-12-18 22:11:58 +0000

    Add test that actually tests CS behaviour, not DRM

--- UnitTests/CompositeStreamableTests.cs
+++ UnitTests/CompositeStreamableTests.cs
@@ -17,13 +17,37 @@ namespace UnitTests
             }
         }
 
-        [TestCase(0.0)]
-        [TestCase(100.0)]
-        [TestCase(1000000.0)]
+        private class ConstStreamable<T> : IStreamable<T>
+        {
+            private T m_Constant;
+            public ConstStreamable(T constant)
+            {
+                m_Constant = constant;
+            }
+
+            public T AtMoment(IStreamMoment moment)
+            {
+                return m_Constant;
+            }
+        }
+
+        [Test]
+        public void AccessingUnassignedRegionThrowsException()
+        {
+            var compositeStreamable = new CompositeStreamable<int>();
+            compositeStreamable.Add(new DoubleStreamMoment(1), new DoubleStreamMoment(6), new ConstStreamable<int>(5));
+            Assert.That(() => compositeStreamable.AtMoment(new DoubleStreamMoment(0)), Throws.InstanceOf<ArgumentException>());
+            Assert.That(() => compositeStreamable.AtMoment(new DoubleStreamMoment(0.99)), Throws.InstanceOf<ArgumentException>());
+            Assert.That(() => compositeStreamable.AtMoment(new DoubleStreamMoment(6)), Throws.InstanceOf<ArgumentException>());
+            Assert.That(() => compositeStreamable.AtMoment(new DoubleStreamMoment(100)), Throws.InstanceOf<ArgumentException>());
+        }
+
+        [Test]
         public void EmptyCompStrAlwaysThrowsException(double time)
         {
             var compositeStreamable = new CompositeStreamable<int>();
-            Assert.That(() => compositeStreamable.AtMoment(new DoubleStreamMoment(time)), Throws.InstanceOf<ArgumentException>());
+            Assert.That(() => compositeStreamable.AtMoment(new DoubleStreamMoment(0)), Throws.InstanceOf<ArgumentException>());
+            Assert.That(() => compositeStreamable.AtMoment(new DoubleStreamMoment(100)), Throws.InstanceOf<ArgumentException>());
         }
 
         [Test]
