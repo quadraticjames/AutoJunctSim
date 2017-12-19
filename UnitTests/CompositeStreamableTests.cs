db386fb0b87a5c413a1ba15b36429857f56c725a
James Wright <jwizzle.uk@gmail.com>
2017-12-19 21:54:48 +0000

    Split off tests that test DRM instead of CS

--- UnitTests/CompositeStreamableTests.cs
+++ UnitTests/CompositeStreamableTests.cs
@@ -51,36 +51,12 @@ namespace UnitTests
         }
 
         [Test]
-        public void CorrectlyInsertsEndlessElement()
+        public void AccessingAssignedRegionReturnsCorrectResult()
         {
             var compositeStreamable = new CompositeStreamable<int>();
-            compositeStreamable.Add(new DoubleStreamMoment(0), new ConstantStreamable<int>(5));
-            Assert.That(compositeStreamable.AtMoment(new DoubleStreamMoment(0)), Is.EqualTo(5));
-            Assert.That(compositeStreamable.AtMoment(new DoubleStreamMoment(100)), Is.EqualTo(5));
-        }
-
-        [Test]
-        public void CorrectlyInsertsEndedElement()
-        {
-            var compositeStreamable = new CompositeStreamable<int>();
-            compositeStreamable.Add(new DoubleStreamMoment(1), new DoubleStreamMoment(2), new ConstantStreamable<int>(5));
-            Assert.That(() => compositeStreamable.AtMoment(new DoubleStreamMoment(0)), Throws.InstanceOf<ArgumentException>());
+            compositeStreamable.Add(new DoubleStreamMoment(1), new DoubleStreamMoment(6), new ConstStreamable<int>(5));
             Assert.That(compositeStreamable.AtMoment(new DoubleStreamMoment(1)), Is.EqualTo(5));
-            Assert.That(compositeStreamable.AtMoment(new DoubleStreamMoment(1.5)), Is.EqualTo(5));
-            Assert.That(() => compositeStreamable.AtMoment(new DoubleStreamMoment(2)), Throws.InstanceOf<ArgumentException>());
-        }
-
-        [Test]
-        public void CorrectlyOverwritesElement()
-        {
-            var compositeStreamable = new CompositeStreamable<int>();
-            compositeStreamable.Add(new DoubleStreamMoment(0), new ConstantStreamable<int>(5));
-            compositeStreamable.Add(new DoubleStreamMoment(1), new DoubleStreamMoment(2), new ConstantStreamable<int>(6));
-            Assert.That(compositeStreamable.AtMoment(new DoubleStreamMoment(0)), Is.EqualTo(5));
-            Assert.That(compositeStreamable.AtMoment(new DoubleStreamMoment(1)), Is.EqualTo(6));
-            Assert.That(compositeStreamable.AtMoment(new DoubleStreamMoment(1.5)), Is.EqualTo(6));
-            Assert.That(compositeStreamable.AtMoment(new DoubleStreamMoment(2)), Is.EqualTo(5));
-            Assert.That(compositeStreamable.AtMoment(new DoubleStreamMoment(3)), Is.EqualTo(5));
+            Assert.That(compositeStreamable.AtMoment(new DoubleStreamMoment(5.99)), Is.EqualTo(5));
         }
     }
 }
---
+++ UnitTests/DoubleRangeMapTests.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using SimBase;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class DoubleRangeMapTests
+    {
+        [Test]
+        public void CorrectlyInsertsEndlessElement()
+        {
+            var map = new DoubleRangeMap<int>();
+            map.Add(0, 5);
+
+            Assert.That(map[0], Is.EqualTo(5));
+            Assert.That(map[1000], Is.EqualTo(5));
+            Assert.That(map[double.MaxValue], Is.EqualTo(5));
+        }
+
+        [Test]
+        public void CorrectlyInsertsEndedElement()
+        {
+            var map = new DoubleRangeMap<int>();
+            map.Add(1, 2, 5);
+
+            Assert.That(map[1], Is.EqualTo(5));
+            Assert.That(map[1.999], Is.EqualTo(5));
+        }
+
+        [Test]
+        public void InitiallyDefaultEverywhere()
+        {
+            var map = new DoubleRangeMap<object>();
+            foreach (var i in new double[] { double.MinValue, 0, double.MaxValue})
+            {
+                Assert.That(map[i], Is.Null);
+            }
+        }
+
+        [Test]
+        public void DefaultCanBeOverridden()
+        {
+            var obj = new DoubleRangeMap<int>();
+            var map = new DoubleRangeMap<object>(obj);
+
+            foreach(var i in new double[] { double.MinValue, 0, double.MaxValue})
+            {
+                Assert.That(map[i], Is.EqualTo(obj));
+            }
+        }
+
+        [Test]
+        public void InsertingEndedElementDoesNotChangeOutsideRange()
+        {
+            var map = new DoubleRangeMap<bool>(false);
+            map.Add(0, 2, true);
+
+            foreach (var i in new double[] { double.MinValue, -1, -0.0001, 2, double.MaxValue })
+            {
+                Assert.That(map[i], Is.False);
+            }
+        }
+
+        [Test]
+        public void InsertingEndlessElementDoesNotChangeOutsideRange()
+        {
+            var map = new DoubleRangeMap<bool>(false);
+            map.Add(0, true);
+
+            foreach (var i in new double[] { double.MinValue, -1, -0.0001 })
+            {
+                Assert.That(map[i], Is.False);
+            }
+        }
+
+        [Test]
+        public void CanOverwriteMultipleElements()
+        {
+            var map = new DoubleRangeMap<int>(0);
+            map.Add(10, 20, 100);
+            map.Add(20, 30, 101);
+            map.Add(30, 40, 102);
+            map.Add(0, 35, 200);
+
+            foreach(var i in new double[] { 0, 10, 20, 30, 34 })
+            {
+                Assert.That(map[i], Is.EqualTo(200));
+            }
+            foreach (var i in new double[] { 35, 39, 39.999 })
+            {
+                Assert.That(map[i], Is.EqualTo(102));
+            }
+        }
+    }
+}
