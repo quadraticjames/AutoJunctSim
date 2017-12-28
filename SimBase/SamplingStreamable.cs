239e65794b49787bd1d83353243a753d70a40492
James Wright <jwizzle.uk@gmail.com>
2017-12-28 21:30:14 +0000

    Add sampling streamable

---
+++ SimBase/SamplingStreamable.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SpatialTypes;
+
+namespace SimBase
+{
+    public class SamplingStreamable<T> : IStreamable<T> where T : IInterpolable<T>
+    {
+        private CompositeStreamable<T> m_Streamable;
+
+        public SamplingStreamable()
+        {
+            m_Streamable = new CompositeStreamable<T>();
+        }
+
+        private bool m_FirstSampleSeen = false;
+        private T m_LastItem;
+        private IStreamMoment m_LastMoment;
+
+        public void AddSample(T item, IStreamMoment moment)
+        {
+            if(m_FirstSampleSeen)
+            {
+                if (moment.PositionInStream <= m_LastMoment.PositionInStream)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(moment), "Samples must be submitted in chronological order");
+                }
+                m_Streamable.Add(m_LastMoment, moment, new InterpolatingStreamable<T>(m_LastItem, item, m_LastMoment, moment));
+            }
+            m_LastItem = item;
+            m_LastMoment = moment;
+            m_FirstSampleSeen = true;
+        }
+
+        public T AtMoment(IStreamMoment moment)
+        {
+            return m_Streamable.AtMoment(moment);
+        }
+    }
+}
---
+++ UnitTests/SamplingStreamableTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SimBase;
+using SpatialTypes;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+
+    [TestFixture]
+    public class SamplingStreamableTests
+    {
+        [Test]
+        public void EmptyStreamableThrowsException()
+        {
+            var streamable = new SamplingStreamable<Point>();
+            foreach (var m in new double[] { 0, 1, 100.5 }.Select(d => new DoubleStreamMoment(d)))
+            {
+                Assert.That(() => streamable.AtMoment(m), Throws.InstanceOf<Exception>());
+            }
+        }
+
+        [Test]
+        public void InterpolatesBetweenTwoSamples()
+        {
+            var streamable = new SamplingStreamable<Point>();
+            streamable.AddSample(new Point(0, 0), new DoubleStreamMoment(1));
+            streamable.AddSample(new Point(1, 1), new DoubleStreamMoment(2));
+
+            var initial = streamable.AtMoment(new DoubleStreamMoment(1));
+            var final = streamable.AtMoment(new DoubleStreamMoment(1.99999999));
+            var middle = streamable.AtMoment(new DoubleStreamMoment(1.5));
+
+            Assert.That(initial.X, Is.EqualTo(0).Within(0.0001));
+            Assert.That(middle.X, Is.EqualTo(0.5).Within(0.0001));
+            Assert.That(final.X, Is.EqualTo(1).Within(0.0001));
+        }
+    }
+}
