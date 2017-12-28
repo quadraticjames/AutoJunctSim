9b24cd5b1e4712289db79fc274c2c0a7d1f0de11
James Wright <jwizzle.uk@gmail.com>
2017-12-28 19:30:56 +0000

    Add StreamableList

---
+++ SimBase/StreamableList.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimBase
+{
+    public class StreamableList<T> : IStreamable<IList<T>>
+    {
+        private class Log
+        {
+            public double Time;
+            public T Item;
+        }
+
+        private List<Log> m_Log = new List<Log>();
+
+        public IList<T> AtMoment(IStreamMoment moment)
+        {
+            var now = moment.PositionInStream;
+            return m_Log.TakeWhile(l => l.Time <= now).Select(l => l.Item).ToList();
+        }
+
+        public void AddAt(T item, IStreamMoment moment)
+        {
+            m_Log.Add(new Log()
+            {
+                Time = moment.PositionInStream,
+                Item = item
+            });
+        }
+    }
+}
---
+++ UnitTests/StreamableListTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using SimBase;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class StreamableListTests
+    {
+        [Test]
+        public void EmptyListAlwaysEmpty()
+        {
+            var list = new StreamableList<int>();
+            foreach(var moment in new double[] { 0, 1, 100 }.Select(d => new DoubleStreamMoment(d)))
+            {
+                Assert.That(list.AtMoment(moment), Is.Empty);
+            }
+        }
+
+        [Test]
+        public void AddedItemReturned()
+        {
+            var list = new StreamableList<int>();
+            list.AddAt(5, new DoubleStreamMoment(0));
+            var expected = new int[] { 5 };
+            foreach (var moment in new double[] { 0, 1, 100 }.Select(d => new DoubleStreamMoment(d)))
+            {
+                Assert.That(list.AtMoment(moment), Is.EquivalentTo(expected));
+            }
+        }
+
+        [Test]
+        public void AddedItemNotReturnedBeforeAddition()
+        {
+            var list = new StreamableList<int>();
+            list.AddAt(5, new DoubleStreamMoment(1));
+            foreach (var moment in new double[] { 0, 0.5, 0.9999 }.Select(d => new DoubleStreamMoment(d)))
+            {
+                Assert.That(list.AtMoment(moment), Is.Empty);
+            }
+        }
+    }
+}
