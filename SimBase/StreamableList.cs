f99343aebe216157d8c7c8eb4d0e979564e2ebdb
James Wright <jwizzle.uk@gmail.com>
2017-12-28 19:50:18 +0000

    Add ability to remove from lists

--- SimBase/StreamableList.cs
+++ SimBase/StreamableList.cs
@@ -12,6 +12,13 @@ namespace SimBase
         {
             public double Time;
             public T Item;
+            public LogType Type;
+
+            public enum LogType
+            {
+                Add,
+                Remove
+            }
         }
 
         private List<Log> m_Log = new List<Log>();
@@ -19,7 +26,22 @@ namespace SimBase
         public IList<T> AtMoment(IStreamMoment moment)
         {
             var now = moment.PositionInStream;
-            return m_Log.TakeWhile(l => l.Time <= now).Select(l => l.Item).ToList();
+            var logs = m_Log.TakeWhile(l => l.Time <= now).ToList();
+            var added = new List<Log>();
+            var removed = new List<Log>();
+            foreach(var log in logs)
+            {
+                if(log.Type == Log.LogType.Add)
+                {
+                    added.Add(log);
+                }
+                else
+                {
+                    removed.Add(log);
+                }
+            }
+            var removedItems = removed.Select(l => l.Item).ToList();
+            return added.Select(l => l.Item).Except(removedItems).ToList();
         }
 
         public void AddAt(T item, IStreamMoment moment)
@@ -27,7 +49,18 @@ namespace SimBase
             m_Log.Add(new Log()
             {
                 Time = moment.PositionInStream,
-                Item = item
+                Item = item,
+                Type = Log.LogType.Add
+            });
+        }
+
+        public void RemoveAt(T item, IStreamMoment moment)
+        {
+            m_Log.Add(new Log()
+            {
+                Time = moment.PositionInStream,
+                Item = item,
+                Type = Log.LogType.Remove
             });
         }
     }
--- UnitTests/StreamableListTests.cs
+++ UnitTests/StreamableListTests.cs
@@ -43,5 +43,32 @@ namespace UnitTests
                 Assert.That(list.AtMoment(moment), Is.Empty);
             }
         }
+        
+        [Test]
+        public void RemovedItemReturnedBeforeRemoval()
+        {
+            var list = new StreamableList<int>();
+            list.AddAt(5, new DoubleStreamMoment(1));
+            list.RemoveAt(5, new DoubleStreamMoment(2));
+            var expected = new int[] { 5 };
+
+            foreach (var moment in new double[] { 1, 1.5, 1.999 }.Select(d => new DoubleStreamMoment(d)))
+            {
+                Assert.That(list.AtMoment(moment), Is.EquivalentTo(expected));
+            }
+        }
+
+        [Test]
+        public void RemovedItemNotReturnedAfterRemoval()
+        {
+            var list = new StreamableList<int>();
+            list.AddAt(5, new DoubleStreamMoment(1));
+            list.RemoveAt(5, new DoubleStreamMoment(2));
+
+            foreach(var moment in new double[] { 2, 3, 100}.Select(d => new DoubleStreamMoment(d)))
+            {
+                Assert.That(list.AtMoment(moment), Is.Empty);
+            }
+        }
     }
 }
