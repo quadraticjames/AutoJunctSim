448d6362c7594d59ac6700f94ba6e4e096d12edf
James Wright <jwizzle.uk@gmail.com>
2017-12-28 20:45:10 +0000

    Remove debugging simulation; start afresh

---
+++ AutoJunctSim/AutoJunctSimulationStream.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SimBase;
+
+namespace AutoJunctSim
+{
+    public class AutoJunctSimulationStream : IAutoJunctSimulationStream
+    {
+        public IStreamable<IList<IStreamable<IVehicleSprite>>> Vehicles => VehiclesList;
+        private StreamableList<IStreamable<IVehicleSprite>> VehiclesList = new StreamableList<IStreamable<IVehicleSprite>>();
+    }
+}
---
+++ AutoJunctSim/IAutoJunctSimulationStream.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SimBase;
+
+namespace AutoJunctSim
+{
+    public interface IAutoJunctSimulationStream
+    {
+        IStreamable<IList<IStreamable<IVehicleSprite>>> Vehicles { get; }
+    }
+}
